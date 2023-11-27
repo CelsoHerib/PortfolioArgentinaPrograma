1ecf3eee43ba50e2e50088faec6d933192ccba36
CelsoHerib <102439750+CelsoHerib@users.noreply.github.com>
2023-11-27 18:51:44 -0300

    header banner

---
+++ PortfolioArgentinaPrograma/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
+import { RedesComponent } from './redes/redes.component';
+import { BannerComponent } from './banner/banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    RedesComponent,
+    BannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
